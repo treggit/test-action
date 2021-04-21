a1854977b0417dda2b3d6705839b44f27e06c7b8
anton.shelepov <anton.shelepov@jetbrains.com>
2021-04-21 15:24:33 +0300

    Test

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const core = require('@actions/core')
-
-core.setSecret("github")
-
-console.log("github is cool!")
-core.setOutput("some-output", "test")
-
-core.startGroup("test_group")
-core.error("Something went wrong!")
-core.warning("Something went wrong but not really disastrous!")
-core.endGroup()
-
-core.exportVariable("TEST_VAR", "test")
-core.addPath("some-path")
+// const core = require('@actions/core')
+//
+// core.setSecret("github")
+//
+// console.log("github is cool!")
+// core.setOutput("some-output", "test")
+//
+// core.startGroup("test_group")
+// core.error("Something went wrong!")
+// core.warning("Something went wrong but not really disastrous!")
+// core.endGroup()
+//
+// core.exportVariable("TEST_VAR", "test")
+// core.addPath("some-path")
 console.log(__dirname)
 // core.setFailed("Fail :(")
